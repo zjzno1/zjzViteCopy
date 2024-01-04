d7f048ebe0955e6d373374177ceab232ea56fc27
ZhaoJizhou <zhaojizhou@AT13396.local>
2024-01-04 15:30:52 +0800

    feat: change import method

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 在`vite.config.js`中引入
 ```
 
-    import { zjzViteCopy } from 'zjzViteCopy';
+    import zjzViteCopy from './zjzViteCopy';
 ```
 
 在plugin中写入:
@@ -10,7 +10,7 @@
 ```
 
     plugins: [
-        zjzViteCopy('./src/assets', './dist/png') // 第一个被拷贝的文件夹，第二个是拷贝到的文件夹，拷贝文件夹中的内容
+        zjzViteCopy.copy('./src/assets', './dist/png') // 第一个被拷贝的文件夹，第二个是拷贝到的文件夹，拷贝文件夹中的内容
     ],
 
 ```
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
-export const zjzViteCopy = (from, to) => {
-  return {
-    name: 'zjz-vite-copy',
-    apply: 'build',
-    buildEnd() {
-      setTimeout(() => {
-        const srcFolder = path.resolve(__dirname, from);
-        const distFolder = path.resolve(__dirname, to);
-        fs.promises.mkdir(distFolder, { recursive: true }) // 创建目标文件夹
-          .then((err, res) => {
-            return fs.promises.readdir(srcFolder);
-          }) // 读取源文件夹
-          .then(files => Promise.all( // 复制所有文件
-            files.map(file => {
-              fs.promises.copyFile(path.join(srcFolder, file), path.join(distFolder, file))
-            }
-            )
-          ))
-          .catch(console.error);
-      }, 1000);
-    },
-  };
-};
+module.exports = {
+  copy:(from, to) => {
+    return {
+      name: 'zjz-vite-copy',
+      apply: 'build',
+      buildEnd() {
+        setTimeout(() => {
+          const srcFolder = path.resolve(process.cwd(), from);
+          const distFolder = path.resolve(process.cwd(), to);
+          fs.promises.mkdir(distFolder, { recursive: true }) // 创建目标文件夹
+            .then((err, res) => {
+              return fs.promises.readdir(srcFolder);
+            }) // 读取源文件夹
+            .then(files => Promise.all( // 复制所有文件
+              files.map(file => {
+                fs.promises.copyFile(path.join(srcFolder, file), path.join(distFolder, file))
+              }
+              )
+            ))
+            .catch(console.error);
+        }, 1000);
+      },
+    };
+  }
+}
